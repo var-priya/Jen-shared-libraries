51dceb57a112c558d82e10b3803e39c2f1099578
Priyanka Varshney <118878221+var-priya@users.noreply.github.com>
2025-10-05 15:45:31 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -3,10 +3,17 @@ def call(Map config = [:]) {
     def imageTag = config.imageTag ?: 'latest'
     def dockerfile = config.dockerfile ?: 'Dockerfile'
     def context = config.context ?: '.'
+    def environment = config.environment ?: 'dev'
     
-    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
-    
+    echo "Building Docker image for ${environment.toUpperCase} : ${imageName}:${imageTag} using ${dockerfile}"
+    try{
     sh """
-        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+        docker build -t ${imageName}:${imageTag} -f ${dockerfile} ${context}
     """
+    echo "Docker image ${imageName}:${imageTag} is successfully build for ${environment.toUpperCase} from ${dockerfile}"
+    }
+    catch(err){
+        echo "Failed to build the image for ${environment.toUpperCase} : ${err.getMessage()}"
+        error("Stopping pipeline as  build is failed ")
+    }
 }
