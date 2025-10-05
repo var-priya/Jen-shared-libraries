59e80e78d2a59cf1d558a4ae7211c04d92281539
Priyanka Varshney <118878221+var-priya@users.noreply.github.com>
2025-10-05 15:09:06 +0530

    Update clean_ws.groovy

--- vars/clean_ws.groovy
+++ vars/clean_ws.groovy
@@ -1,4 +1,12 @@
-def call() {
-    echo "Cleaning up workspace..."
-    cleanWs()
+def call(Map Config = [:]) {
+    def when = config.when ?: 'pre'
+    def environment = config.environment ?: 'dev'
+    echo "Cleaning up workspace for ${environment.toUpperCase()}(${when})"
+    try{
+        cleanWs(deleteDirs: true, notFailBuild: true) 
+        echo "Workspace cleaned successfully for ${environment.toUpperCase()}"
+    }
+    catch(err){
+        echo "Failed to clean Workspace for ${environment.toUpperCase()} : ${err.getMessage()}"
+    }
 }
