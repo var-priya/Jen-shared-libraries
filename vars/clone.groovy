fb9032bff2e0ade5633a412d70916c2666d3490e
Priyanka Varshney <118878221+var-priya@users.noreply.github.com>
2025-10-05 14:42:23 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,3 +1,16 @@
-def call(String url, String branch){
-  git url: "${url}", branch: "${branch}"
-}
+def call(Map Config = [:]){
+  def repoUrl = config.url ?: 'https://github.com/var-priya/E-commerce-application.git'
+  def branchName = config.branchName ?: 'master'
+  def credentialsId = config.credentialsId ?: ''
+  def environment = config.environment ?: 'dev'
+
+  try{
+    echo "Checking out code for ${environment.toUpperCase()} from ${repoUrl} (branch: ${branchName})"
+    git url: repoUrl, branch: branchName, credentialsId: credentialsId
+    echo "Checkout successfull for ${environment.toUpperCase()}"
+  }
+  catch(err){
+    echo "Failed to Checkout for ${environment.toUpperCase()} : ${err.getMessage()}"
+    error("Stopping pipeline due to failure Checkout for ${envorinment.toUpperCase()})
+  }
+  
