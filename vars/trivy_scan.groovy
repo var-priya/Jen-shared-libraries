68f88449f76c883a9375a50a523d9dde8e5ffc61
Priyanka Varshney <118878221+var-priya@users.noreply.github.com>
2025-10-05 17:03:24 +0530

    Update trivy_scan.groovy

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,3 +1,28 @@
-def call(){
-  sh "trivy fs ."
+def call(Map params = [:]) {
+    // ----------------------------
+    // Required and optional parameters
+    // ----------------------------
+    def imageName = params.imageName ?: error("🚨 Docker image name is required for Trivy scan")
+    def imageTag = params.imageTag ?: 'latest'
+    def severity = params.severity ?: 'CRITICAL,HIGH,MEDIUM' // Trivy severity levels to fail on
+    def ignoreUnfixed = params.ignoreUnfixed ?: false
+    def format = params.format ?: 'table' // json/table/sarif
+    def exitCodeOnVuln = params.exitCodeOnVuln ?: 1
+    def environment = params.environment ?: 'dev'
+
+    def fullImage = "${imageName}:${imageTag}"
+    def ignoreFlag = ignoreUnfixed ? '--ignore-unfixed' : ''
+
+
+    echo "Running Trivy vulnerability scan on ${fullImage} for environment: ${environment.toUpperCase()}"
+
+    try {
+        
+        sh "trivy image --exit-code ${exitCodeOnVuln} --severity ${severity} ${ignoreFlag} --format ${format} ${fullImage}"
+
+        echo "Trivy scan completed successfully for ${fullImage} in ${environment.toUpperCase()}"
+    } catch (err) {
+        echo " Trivy scan failed or found vulnerabilities in ${fullImage}: ${err.getMessage()}"
+        error("Stopping pipeline due to Trivy scan failure")
+    }
 }
